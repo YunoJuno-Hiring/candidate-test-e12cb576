4c50e209868a409cf39f33163aeb12725af771c0
Ian Culshaw <culshaw@gmail.com>
2021-11-29 16:14:57 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import styles from "./App.module.css";
 
+// Character list is available in the public directory
+
 class App extends Component {
     render() {
         return (
